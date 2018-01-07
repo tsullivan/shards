83174ffe9c9c169b82b5de5680caf79f868140fb
Timothy Sullivan <tsullivan@elastic.co>
2018-01-07 13:54:10 -0700

    better size sorting for "biggest", more output properties

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-/*eslint-disable no-console*/
 const { groupBy, last } = require('lodash');
 const shards = require('./shards');
 
-const sizes = groupBy(shards, (s) => {
+const sizeGroups = groupBy(shards, (s) => {
   const gb = s.store / 1000 / 1000 / 1000; // convert bytes to gb
-  return Math.ceil(gb / 50) * 50; // round s.store to the nearest 50gb
-});
-const states = groupBy(shards, (s) => {
-  return s.prirep + '-' + s.state;
+  const group = Math.ceil(gb / 50) * 50; // round s.store to the nearest 50gb
+  return group + 'gb';
 });
 
-const sizeKeys = Object.keys(sizes);
+const stateGroups = groupBy(shards, (s) => s.prirep + '-' + s.state);
 
-console.log(JSON.stringify({
-  biggest: sizes[last(sizeKeys)],
-  sizes,
-  states,
-}));
+const sizeKeys = Object.keys(sizeGroups).map((k) => parseInt(k, 10));
+sizeKeys.sort();
+
+const numShardsPerGroup = sizeKeys.reduce((accum, curr) => {
+  const currGroup = curr + 'gb';
+  return Object.assign({},
+    accum,
+    { [currGroup]: sizeGroups[currGroup].length }
+  );
+}, {});
+
+const biggest = {
+  group: last(sizeKeys) + 'gb',
+  shards: sizeGroups[last(sizeKeys) + 'gb'],
+};
+
+//eslint-disable-next-line no-console
+console.log(
+  JSON.stringify({
+    num_shards_total: shards.length,
+    shards_by_group: sizeGroups,
+    states: stateGroups,
+    num_shards_per_group: numShardsPerGroup,
+    biggest,
+  })
+);

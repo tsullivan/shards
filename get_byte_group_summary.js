b7b414fad68d27ad42ad276b08c4ff437a345eb6
Timothy Sullivan <tsullivan@elastic.co>
2018-01-07 18:01:14 -0700

    more reasonable setting to group shards into gb groups

--- get_byte_group_summary.js
+++ get_byte_group_summary.js
@@ -1,9 +1,11 @@
 const { groupBy, last } = require('lodash');
 
+const GROUP_BY = 10; // groups lists of shards by this many gigabytes
+
 function getByteProperties(shards) {
   const byteGroups = groupBy(shards, (s) => {
     const gb = s.store / 1000 / 1000 / 1000; // convert bytes to gb
-    const group = Math.ceil(gb / 50) * 50;
+    const group = Math.ceil(gb / GROUP_BY) * GROUP_BY;
     return group;
   });
 
